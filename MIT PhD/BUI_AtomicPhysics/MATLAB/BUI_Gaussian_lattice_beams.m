5d039f032ae64c31e7121528369807e0cb34a3f3
Huan Bui <44960171+huanium@users.noreply.github.com>
2021-07-21 02:25:07 -0400

    j

--- MIT PhD/BUI_AtomicPhysics/MATLAB/BUI_Gaussian_lattice_beams.m
+++ MIT PhD/BUI_AtomicPhysics/MATLAB/BUI_Gaussian_lattice_beams.m
@@ -48,14 +48,10 @@ tic
 %%%%% Real Gaussian profiles %%%%%%%%%%%%%
 
 global wavelength; % wavelength
-global w0; % beam waist
-global A0; % electric field amplitude
-global zR; % Rayleigh range
 global n; % index of refraction of air
-global theta; % beam divergence
 global focusX; % waist location along X
 global focusZ; % waist location along Z
-global theta_inc; % incoming beam incidence angle
+global focusY;
 %%% retro mirror position and angle
 global retro_X_intercept
 global retro_angle % relative to horizontal
@@ -64,43 +60,50 @@ global retro_angle % relative to horizontal
 
 %%%%% SETTING UP %%%%%
 wavelength= 1064e-9; % in meters
-w0 = 40e-6; % beam waist in meters
-A0 = 1; % arbitrary...
+w0_x = 40e-6; % beam waist (in X) in meters
+w0_y = 140e-6; % beam waist (in Y) in meters
 n = 1; % of air
-zR = pi*w0^2*n/wavelength;
-theta = wavelength/(n*w0); % beam divergence, in rads
-theta_inc = -10.8*pi/180; % 5 degs from horizontal
+zR_x = pi*w0_x^2*n/wavelength; % Rayleigh range X
+zR_y = pi*w0_y^2*n/wavelength; % Rayleigh range Y
+theta_X = wavelength/(n*w0_x); % beam divergence in X, in rads
+theta_Y = wavelength/(n*w0_y); % beam divergence in X, in rads
+theta_inc_X = -10.8*pi/180; % 10.8 degs from horizontal
 
 % focus point defined along path of incoming beam 
-focusZ = 5e-3; % Z-position of focus (5mm above surface)
+focusZ = 3e-3; % Z-position of focus (5mm above surface)
 % set X-position of focus so that the reflection occurs at the origin
-focusX = cot(-theta_inc)*focusZ;  % X-position of focus
+focusX = cot(-theta_inc_X)*focusZ;  % X-position of focus
+focusY = 0;
 
 % retro mirror parameters
 retro_X_intercept = 0.75e-3; % kinda far from incidence spot
 retro_misalign = 0*pi/180; % deviation from perfect retroreflection
-retro_angle = pi/2 + theta_inc; % gives perfect retro-reflection
+retro_angle = pi/2 + theta_inc_X; % gives perfect retro-reflection
 retro_angle = retro_angle - retro_misalign;
 
 % input range 
-res_Z = 1e-9; % resolution Z
-res_X = 2e-6; % resolution X
-x_bound = 400e-6;
-z_bound = 10e-5;
+res_Z = 5e-9; % resolution Z
+res_X = 1e-7; % resolution X
+res_Y = 1e-7;
+x_bound = 200e-6;
+y_bound = 200e-6;
+z_bound = 2e-5;
 x = -x_bound:res_X:x_bound;
 z = -z_bound:res_Z:0;
+y = -y_bound:res_Y:y_bound;
 %z = -13e-6:res_Z:-12.5e-6; % THIS IS THE LAYER WHERE THE ATOMS ARE
 [X, Z] = meshgrid(x, z);
 
 Efield = @E;
 % rotate coords, then make E field (incoming)
 % incoming beam, start with original w0 and zR
-E_inc = Efield(w0, zR, new_X(X-focusX,Z-focusZ,theta_inc),new_Z(X-focusX,Z-focusZ,theta_inc));
+E_inc = Efield(w0_x, zR_x, new_X(X-focusX,Z-focusZ,theta_inc_X),...
+    new_Z(X-focusX,Z-focusZ,theta_inc_X));
 
 % reflection of incoming beam 
-[eff_focus_X_refl, eff_focus_Z_refl, theta_inc_refl] = refl_focus();
-E_refl = Efield(w0, zR, new_X(X-eff_focus_X_refl,Z-eff_focus_Z_refl,theta_inc_refl),...
-    new_Z(X-eff_focus_X_refl,Z-eff_focus_Z_refl,theta_inc_refl));
+[eff_focus_X_refl, eff_focus_Z_refl, theta_inc_refl] = refl_focus(theta_inc_X);
+E_refl = Efield(w0_x, zR_x, new_X(X-eff_focus_X_refl,Z-eff_focus_Z_refl,...
+    theta_inc_refl),new_Z(X-eff_focus_X_refl,Z-eff_focus_Z_refl,theta_inc_refl));
 
 % retro beam: without focusing lens on retroreflection
 % [eff_focus_X_retro, eff_focus_Z_retro, theta_reflection] = retro_focus();
@@ -125,15 +128,15 @@ pattern = abs(E_inc - E_refl - E_ret + E_ret_refl).^2;
 h = figure(1);
 h.Position = [200 -10 800 800];
 % top view of surface plot to look at XZ plane
-subplot(4,1,1)
+subplot(3,1,1)
 surf(X,Z,pattern, 'LineStyle', 'none', 'EdgeAlpha', 0 , 'FaceAlpha',1);
 colormap('gray');
 ylim([-z_bound 0]); % this sets the 'Z' bound...
 view([0,90])
 xlabel('X (m)')
 ylabel('Z (m)')
-title({['Gaussian beam waist: ' num2str(w0) ' m, Wavelength: ' num2str(wavelength) ' m'],...
-    ['Incidence: ' num2str(-theta_inc*180/pi)...
+title({['Gaussian beam waist: ' num2str(w0_x) ' m, Wavelength: ' num2str(wavelength) ' m'],...
+    ['Incidence: ' num2str(-theta_inc_X*180/pi)...
     ' degs, Focus: X = ' num2str(round(focusX,3)) ' m, Z = ' num2str(focusZ) ' m']});
 
 % % side view
@@ -147,7 +150,7 @@ title({['Gaussian beam waist: ' num2str(w0) ' m, Wavelength: ' num2str(wavelengt
 % zlabel('Intensity (a.u.)')
 
 % zoom in close to atom layer near surface
-subplot(4,1,2)
+subplot(3,1,2)
 surf(X,Z,pattern, 'LineStyle', 'none', 'EdgeAlpha', 0 , 'FaceAlpha',1);
 colormap('gray');
 %z = -13e-6:res_Z:-12.5e-6; % THIS IS THE LAYER WHERE THE ATOMS ARE
@@ -157,58 +160,25 @@ xlabel('X (m)')
 ylabel('Z (m)')
 title('Zoom in to atom layer near surface, z~13e-6 m (~3rd layer)')
 
-% slice at x = 0
-subplot(4,1,3)
-x0_data = pattern(:,floor(x_bound/res_X));
-plot(z,x0_data);
-xlim([-z_bound 0])
-title('Intensity profile across x = 0.')
-set ( gca, 'xdir', 'reverse' )
-xlabel('Z (m)')
-ylabel('Intensity (a.u.)')
+% % slice at x = 0
+% subplot(4,1,3)
+% x0_data = pattern(:,floor(x_bound/res_X + 1));
+% plot(z,x0_data);
+% xlim([-z_bound 0])
+% title('Intensity profile across x = 0.')
+% set ( gca, 'xdir', 'reverse' )
+% xlabel('Z (m)')
+% ylabel('Intensity (a.u.)')
 
-subplot(4,1,4)
-% slice at x = 0 near atom layer
-% pick center x=0 and look at z data there
-x_interest0 = 0;
-x0_data = pattern(:,floor((x_bound+x_interest0)/res_X));
-[m0, i0] = max(x0_data);
-disp(['Max intensity at x = ' num2str(x_interest0) 'm = ' num2str(m0)]);
-% z-position of max intensity
-disp(['z = ' num2str(i0) 'm']);
-disp('-----')
-plot(z,x0_data);
-xlim([-14e-6 -11.5e-6])
 
-hold on
-% pick x=?, and look at intensity data there
-x_interest1 = 0 - 2e-4; 
-x1_data = pattern(:,floor((x_bound+x_interest1)/res_X));
-[m1, i1] = max(x1_data);
-disp(['Max intensity at x = ' num2str(x_interest1) 'm = ' num2str(m1)]);
-% z-position of max intensity
-disp(['z = ' num2str(i1) 'm']);
-disp('-----')
-plot(z,x1_data);
-xlim([-14e-6 -11.5e-6])
-hold on
-% pick x=?, and look at intensity data there
-x_interest2 = 0 + 2e-4; 
-x2_data = pattern(:,floor((x_bound+x_interest2)/res_X));
-[m2, i2] = max(x2_data);
-disp(['Max intensity at x = ' num2str(x_interest2) 'm = ' num2str(m2)]);
-% z-position of max intensity
-disp(['z = ' num2str(i2) 'm']);
-disp('-----')
-plot(z,x2_data);
-xlim([-14e-6 -11.5e-6])
-hold off
-
-title('Intensity profile across near atom layer.')
-set ( gca, 'xdir', 'reverse' )
-xlabel('Z (m)')
-ylabel('Intensity (a.u.)')
-legend('x=0m' , ['x=' num2str(x_interest1) 'm'], ['x=' num2str(x_interest2) 'm'])
+% Z position of max intensity across X
+subplot(3,1,3)
+z_interest = -14e-6:res_Z:-11.5e-6;
+[max_longX, indexZ] = max(pattern(floor((z_bound+z_interest)/res_Z+1),:));
+plot(x,z_interest(indexZ));
+xlabel('X (m)')
+ylabel('Z_{max} (m)')
+title('Z position of max intensity across X')
 
 
 
@@ -254,26 +224,25 @@ function new_Z = new_Z(X,Z,theta)
 end
 
 % work out where imaged focus point is 
-function [eff_focus_X, eff_focus_Z, theta_reflection] = retro_focus()
+function [eff_focus_X, eff_focus_Z, theta_reflection] = retro_focus(theta_inc_X)
     global retro_X_intercept
     global retro_angle 
-    global theta_inc
     global focusX
     global focusZ
     
     % incident beam center
-    % focus_Z = tan(-theta_inc)*(focus_X - x_inter)
-    x_inter_incident = focusX - focusZ/tan(-theta_inc);
-    z_inter_incident = tan(-theta_inc)*(0-x_inter_incident);
+    % focus_Z = tan(-theta_inc_X)*(focus_X - x_inter)
+    x_inter_incident = focusX - focusZ/tan(-theta_inc_X);
+    z_inter_incident = tan(-theta_inc_X)*(0-x_inter_incident);
     
     % reflected beam center is given by 
-    % z(x) = tan(theta_inc)*(x - x_inter_incident);
+    % z(x) = tan(theta_inc_X)*(x - x_inter_incident);
     
     % this beam intersects with the mirror, given by 
     % z(x) = tan(retro_angle)*(x - retro_X_intercept);
     
     % so the intersection is given by 
-    x_intersection = retro_X_intercept/(1-tan(theta_inc)/tan(retro_angle));
+    x_intersection = retro_X_intercept/(1-tan(theta_inc_X)/tan(retro_angle));
     z_intersection = tan(retro_angle)*(x_intersection - retro_X_intercept);
     
     % to find the location of the imaged focus point, we have to find the
@@ -294,23 +263,19 @@ end
 
 
 % effective focus point for reflection of incoming beam 
-function [eff_focus_X_refl, eff_focus_Z_refl, theta_inc_refl] = refl_focus()
-    global theta_inc
+function [eff_focus_X_refl, eff_focus_Z_refl, theta_inc_refl] = refl_focus(theta_inc_X)
     global focusX
     global focusZ
     
     % obtained by just flipping the incoming focus point around horz
     eff_focus_X_refl = focusX;
     eff_focus_Z_refl = -focusZ;
-    theta_inc_refl = -theta_inc;
+    theta_inc_refl = -theta_inc_X;
     
 end
 
 % complex beam parameter function
-function beam_parameter_q = q(z)
-    % global wavelength
-    % global n
-    global zR
+function beam_parameter_q = q(z, zR)
     % beam_parameter_q = 1/(1/R(z) - 1i*wavelength/(n*pi*w(z)^2));
     beam_parameter_q = z + 1i*zR; % equivalent defn
 end
@@ -320,14 +285,25 @@ end
 % see page 8/44 of 
 % http://ecee.colorado.edu/~ecen5616/WebMaterial...
 % .../24%20Gaussian%20beams%20and%20Lasers.pdf
-function E_field = E(w0, zR, x,z)
-    % beam propagates mostly in the x direction
-    global A0
+function E_field = E(w0, zR, x, z)
+    % beam propagates mostly in the x direction, so "z" in defns become "x"
     global wavelength
     k = 2*pi/wavelength;
     % electric field in 1 transverse dimension only 
-    E_field = 1i.*A0.*sqrt(1./(sqrt(pi).*w0)).*(zR./q(sqrt(z.^2+x.^2)))...
-        .*exp(-1i.*k.*z.^2./(2.*q(sqrt(z.^2+x.^2)))-1i.*k.*x);
+    E_field = 1i.*sqrt(1./(sqrt(pi).*w0)).*(zR./q(abs(x),zR))...
+        .*exp(-1i.*k.*z.^2./(2.*q(abs(x),zR))-1i.*k.*x);
+end
+
+
+function E_3d = E_field_3d(w0_x,w0_y, zR_x, zR_y, x,y,z)
+    % beam propagates mostly in the x direction
+    global wavelength
+    k = 2*pi/wavelength;
+    % electric field in 1 transverse dimension only 
+    E_field_x = E_field(w0_x, zR_x, x, z);
+    E_field_y = E_field(w0_y, zR_y, y, z);
+    
+    E_3d = E_field_x*E_field_y;
 end
 
 
