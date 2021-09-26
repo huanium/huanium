7b8019b15d96c3e927901cbdcb1ef1e6150580fc
Huan <buiquanghuan1904@gmail.com>
2021-09-26 18:59:40 -0400

    psets

--- MIT PhD/Courses/2021-2022/Fall21-8-309-Classical3/Pset2/Pset_2_Problem_6.m
+++ MIT PhD/Courses/2021-2022/Fall21-8-309-Classical3/Pset2/Pset_2_Problem_6.m
@@ -1,9 +1,8 @@
-theta0 = pi/10;
-L = 3;
-g=9.8;
+theta0 = pi/3;
+L = 100;
+g= 9.8;
 time = 0.563257;
-[t,y] = ode45(@f,[time 0.65],[asin(2*sin(theta0)/3); -sqrt((3*g/L)*(sin(theta0)-(2/3)*sin(theta0)))]);
-
+[t,y] = ode45(@f,[time 0.95],[asin(2*sin(theta0)/3); -sqrt((3*g/L)*(sin(theta0)-(2/3)*sin(theta0)))]);
 
 plot(t,y(:,1),'-o')
 title('Solution');
@@ -13,7 +12,7 @@ legend('y_1')
 
 function dydt = f(t,y)
 g = 9.8;
-L = 3;
-dydt = [y(2); 3*cos(y(1))*(-2*g/L+sin(y(1))*y(2)^2)];
+L = 100;
+dydt = [y(2); 3*cos(y(1))*(-2*g/L+sin(y(1))*y(2)^2)/(3*cos(y(1))^2+1)];
 
 end
