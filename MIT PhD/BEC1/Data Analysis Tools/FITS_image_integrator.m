a9f7e1a5b68deb121681cdb43680761572e85145
Huan Bui <buiquanghuan1904@gmail.com>
2021-10-19 22:11:11 -0400

    bec1

--- MIT PhD/BEC1/Data Analysis Tools/FITS_image_integrator.m
+++ MIT PhD/BEC1/Data Analysis Tools/FITS_image_integrator.m
@@ -6,23 +6,30 @@ clear all
 % prompt  = 'Put folder directory here: ';
 % folderDir = input(prompt, 's');
 
-inputDir = '~/huanium/MIT PhD/BEC1/2021-10-18-catch-wait/2021-10-18_17-20-42.fits';
-data = fitsread(inputDir, 'primary');
+% inputDir = '~/huanium/MIT PhD/BEC1/2021-10-18-catch-wait/2021-10-18_17-20-42.fits';
+target_folder = uigetdir('C:/');
+filePattern = fullfile(target_folder, '*.fits');
+files = dir(filePattern);
+data = fitsread(fullfile(files(1).folder, files(1).name), 'primary');
+
+disp(files(1).name)
 % data has 3 images
 % first image:
 wA = data(:,:,1);
 woA = data(:,:,2);
 dark = data(:,:,3);
+OD = real(-log((data(:,:,1) - data(:,:,3))./(data(:,:,2) - data(:,:,3))));
 
-% OD: OD image = ???
-OD = data(:,:,2) - data(:,:,1);
-
-
+OD(isinf(OD)) = 1;
+OD(isnan(OD)) = 0;
+OD = max(OD,0);
+OD = OD/max(OD(:));
+imshow(OD);
 % re-scale
-wA = wA/255; 
-woA = woA/255; 
-dark = dark/255; 
-OD = OD/255; 
+%wA = wA/255; 
+%woA = woA/255; 
+%dark = dark/255; 
+%OD = OD/max(OD(:)); 
 
 % figure(1)
 % imshow(wA);
@@ -31,16 +38,14 @@ OD = OD/255;
 % figure(3)
 % imshow(dark);
 % figure(4)
-imshow(OD);
-
+% imshow(OD);
 
 % integrating ROI 
 roi = drawrectangle;
-%r = floor(roi.Vertices(:,2)) + 1;
-%c = floor(roi.Vertices(:,1)) + 1;
-r = [401 548 548 401]; % default values for testing 
-c = [461 461 991 991]; % default values for testing
+r = floor(roi.Vertices(:,2)) + 1;
+c = floor(roi.Vertices(:,1)) + 1;
+% r = [455 513 513 455]; % default values for testing 
+% c = [560 560 901 901]; % default values for testing
 J = integralImage(OD);
 regionSum = J(r(1),c(1)) - J(r(2),c(2)) + J(r(3),c(3)) - J(r(4),c(4));
-
 disp(['Region sum is: ', num2str(regionSum)])
--- MIT PhD/BEC1/Data Analysis Tools/FITS_image_integrator_data.m
+++ MIT PhD/BEC1/Data Analysis Tools/FITS_image_integrator_data.m
@@ -2,10 +2,12 @@
 %%% Date: Oct 19, 2021
 %%% Primitive FITS image display/integrator
 
+clear all
+clear crc 
 
 % load images from a specified folder
 regionSums = [];
-target_folder = uigetdir('~/huanium/MIT PhD/BEC1/');
+target_folder = uigetdir('C:/');
 filePattern = fullfile(target_folder, '*.fits');
 files = dir(filePattern);
 for k = 1 : length(files)
@@ -16,25 +18,29 @@ for k = 1 : length(files)
     
     % Now process
     data_k = fitsread(fullFileName, 'primary');
-    
     % get OD image
-    OD = data_k(:,:,2) - data_k(:,:,1);
-    % re-scale
-    OD = OD/255;
-    
+    OD = real(-log((data_k(:,:,1) - data_k(:,:,3))./(data_k(:,:,2) - data_k(:,:,3))));
+    % re-normalize and re-scale
+    OD(isinf(OD)) = 1;
+    OD(isnan(OD)) = 0;
+    OD = max(OD,0);
+    %OD = OD/max(OD(:));
+    % imshow(OD)
     % ROI set to be following (assuming catch didn't move much)
-    r = [401 548 548 401]; % default values for testing 
-    c = [461 461 991 991]; % default values for testing
+    %roi = drawrectangle;
+    %r = floor(roi.Vertices(:,2)) + 1;
+    %c = floor(roi.Vertices(:,1)) + 1;
+    r = [448 519 519 448]; % default values for testing 
+    c = [574 574 921 921]; % default values for testing
     J = integralImage(OD);
     regionSums(k) = J(r(1),c(1)) - J(r(2),c(2)) + J(r(3),c(3)) - J(r(4),c(4));
 end
 
 % disp(regionSums)
-times = [0.1 0.1 0.1 10 10 10 20 20 20 40 40 40 80 80 80];
-
+%times = [0.1 0.1 10 10 20 20 40 40 80 80 100 100 120 120 150 150];
+times = [0.1 0.1 0.1 0.1 10 10 10 10 20 20 20 40 40 40 40 60 60 60 60 80 80 80 100 100 100 100 120 120 120 120];
 % fit now
 f = fit(times',regionSums','exp1');
-
 % plot now
 plot(f)
 hold on
@@ -44,6 +50,7 @@ hold off
 legend('fit', 'data')
 ylabel('Sum of pixel values (a.u.)')
 xlabel('Time (s)')
+title('Catch lifetime, 1s MOT loading')
 
 % extract lifetime out of fit
 coeffs = coeffvalues(f);
